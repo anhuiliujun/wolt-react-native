4343b95d0b5dd1049cd1055d8b2f135f31cf0583
issac <issaclau3@gmail.com>
2025-12-06 17:14:02 +0800

    List of categories and restaurants.

--- app/(app)/(auth)/(tabs)/restaurants/_layout.tsx
+++ app/(app)/(auth)/(tabs)/restaurants/_layout.tsx
@@ -1,8 +1,8 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 const Layout = () => {
   return (
-    <Stack screenOptions={{ contentStyle: { backgroundColor: '#fff' } }}>
-      <Stack.Screen name="index" />
+    <Stack screenOptions={{ contentStyle: { backgroundColor: "#fff" } }}>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 };
